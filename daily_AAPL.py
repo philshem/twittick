9cf50775a8139d8416540e7bfd629fa800ae5fbc
philshem <psny18@gmail.com>
2013-09-01 14:35:44 +0200

    python script

--- daily_AAPL.py
+++ daily_AAPL.py
@@ -2,25 +2,12 @@
 #
 # Copyright 2013 Philip Shemella
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-#'''A library that provides a Python interface to the Twitter API'''
 
 __author__ = 'psny18@gmail.com'
 __version__ = '0.0.1'
 
-''' this code takes a stock quote from markitondemand's API and posts it to twitter.
-    the following code is designed for Apple stock (AAPL).''' 
+#    this code takes a stock quote from markitondemand's API and posts it to twitter.
+#    the following code is designed for Apple stock (AAPL).
 
 import oauth2 as oauth
 import requests
